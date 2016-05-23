d9401d7faf89473318791783a6d3565d915285d1
AdrieDonker <adrie.donker@gmail.com>
2016-05-23 15:43:46 +0200

    Weer Bijgewerkt 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Start hier weer en weer
+  # Start hier weer en weer(3)
 end
